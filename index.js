7bbf88ef4a8926db69caf2fa4761bd4c0ede1091
phoebe12345 <phoebehan888@gmail.com>
2024-12-29 21:02:16 -0800

    Fixed port issue for Heroku deployment

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const path = require('path')
 const cors = require('cors');
 
-const port = process.env.PORT || 5006
+const port = process.env.PORT || 5000
+
 const messages = [
   {author:'one', message:"hello000000", timestamp: 0},
   {author:'two', message:"hello hello", timestamp: 2},
@@ -11,6 +12,9 @@ const messages = [
 const app = express()
 app.use(cors());
 app.use(express.json());
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 // Serve the static files from the React build folder
 app.use(express.static(path.join(__dirname, 'build')));
